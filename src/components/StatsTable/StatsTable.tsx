6f261dd470e6d090bfb37953ebb592ca4116b3c6
Jakub Karaś <jakub.karas@onet.pl>
2023-12-15 16:50:27 +0100

    Added tanstack table and fetching logic implementation

--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -1,7 +1,88 @@
 import React from "react"
+import arrow_right_circle from "assets/icons/arrow_right_circle.png"
+import add_icon from "assets/icons/add_icon.png"
+import { useMemo } from "react"
+import { columns, MatchesStats } from "./StatsTableData"
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { useStatsTableHandler } from "api/statsTable/stastTableHooks"
 
 const StatsTable = () => {
-  return <div>StatsTable</div>
+  const { data: statsTableData, isError, isFetching } = useStatsTableHandler()
+  const data = statsTableData || []
+  const columnsMemo = useMemo(() => columns, [])
+
+  const table = useReactTable({
+    data,
+    columns: columnsMemo,
+    getCoreRowModel: getCoreRowModel<MatchesStats>(),
+  })
+  return (
+    <div className="w-full max-w-6xl">
+      <div className="w-full max-w-6xl bg-white rounded-2xl shadow-lg">
+        <div className="p-4 bg-white border-b-2 border-[#D5E0E8]">
+          <div className="flex justify-between">
+            <div className="flex items-center justify-center gap-4">
+              <img src={add_icon} alt="Ikona plusa" />
+              <h1 className="font-primary text-xl font-medium">
+                Anglia: Premier League
+              </h1>
+            </div>
+            <div className="flex items-center justify-center gap-1">
+              <h2 className="font-primary text-base font-medium">Mecze</h2>
+              <img src={arrow_right_circle} alt="Ikona strzałki" />
+            </div>
+          </div>
+        </div>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr
+                key={headerGroup.id}
+                className="p-4 bg-white border-b-2 border-[#D5E0E8]"
+              >
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      className="p-4 font-primary text-base font-medium text-black opacity-50"
+                      key={header.id}
+                    >
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  )
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {isError && <p>Wystąpił błąd</p>}
+    </div>
+  )
 }
 
 export default StatsTable
---
+++ src/components/StatsTable/StatsTableData.tsx
@@ -0,0 +1,115 @@
+import { createColumnHelper } from "@tanstack/react-table"
+import React from "react"
+export interface MatchesStats {
+  team: {
+    id: number
+    name: string
+    image: string
+  }
+  points: number
+  goals_scored: number
+  goals_conceded: number
+  goals_ratio: number
+  games: number
+}
+
+const columnHelper = createColumnHelper<MatchesStats>()
+
+export const columns = [
+  columnHelper.accessor("team.id", {
+    header: () => <h3 className="font-primary text-xs font-medium">LP.</h3>,
+    cell: (props) => (
+      <p className="font-primary text-xs font-medium">{props.getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor("team.name", {
+    header: () => <h3 className="font-primary text-xs font-medium">DRUŻYNA</h3>,
+    cell: (props) => (
+      <p className="font-primary text-xs font-medium">{props.getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor("games", {
+    header: () => <h3 className="font-primary text-xs font-medium">M</h3>,
+    cell: (props) => (
+      <p className="font-primary text-xs font-medium">{props.getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor((row) => `${row.goals_scored}:${row.goals_conceded}`, {
+    id: "goals",
+    header: () => <h3 className="font-primary text-xs font-medium">B</h3>,
+    cell: (info) => (
+      <p className="font-primary text-xs font-medium">{info.getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor("goals_ratio", {
+    header: () => <h3 className="font-primary text-xs font-medium">RB</h3>,
+    cell: (props) => (
+      <p className="font-primary text-xs font-medium">{props.getValue()}</p>
+    ),
+  }),
+  columnHelper.accessor("points", {
+    header: () => <h3 className="font-primary text-xs font-medium">P</h3>,
+    cell: (props) => (
+      <p className="font-primary text-xs font-medium">{props.getValue()}</p>
+    ),
+  }),
+]
+
+export const MatchesContentData = [
+  {
+    team: {
+      id: 1,
+      name: "Arsenal",
+      image:
+        "https://static.flashscore.com/res/image/data/0n1ffK6k-vcNAdtF9.png",
+    },
+    points: 30,
+    goals_scored: 27,
+    goals_conceded: 10,
+    goals_ratio: 17,
+    games: 13,
+  },
+  {
+    team: {
+      id: 2,
+      name: "Manchester City",
+      image:
+        "https://static.flashscore.com/res/image/data/0vgscFU0-lQuhqN8N.png",
+      created_at: "2023-11-28T12:07:23.000000Z",
+      updated_at: "2023-11-28T12:07:23.000000Z",
+    },
+    points: 29,
+    goals_scored: 33,
+    goals_conceded: 13,
+    goals_ratio: 20,
+    games: 13,
+  },
+  {
+    team: {
+      id: 3,
+      name: "Arsenal",
+      image:
+        "https://static.flashscore.com/res/image/data/0n1ffK6k-vcNAdtF9.png",
+    },
+    points: 30,
+    goals_scored: 23,
+    goals_conceded: 23,
+    goals_ratio: 45,
+    games: 23,
+  },
+  {
+    team: {
+      id: 4,
+      name: "Manchester City",
+      image:
+        "https://static.flashscore.com/res/image/data/0vgscFU0-lQuhqN8N.png",
+      created_at: "2023-11-28T12:07:23.000000Z",
+      updated_at: "2023-11-28T12:07:23.000000Z",
+    },
+    points: 34,
+    goals_scored: 43,
+    goals_conceded: 23,
+    goals_ratio: 34,
+    games: 34,
+  },
+]
