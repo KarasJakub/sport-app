4b0ea484a792cfffd13117fe191ed613add358be
Jakub Karaś <jakub.karas@onet.pl>
2023-12-16 22:00:38 +0100

    Minor fixes after PR

--- src/api/mainMatches/mainMatches.ts
+++ src/api/mainMatches/mainMatches.ts
@@ -2,7 +2,7 @@ import { apiClient } from "api/client"
 
 export const mainMatchesHandler = async (pageNumber: number) => {
     try {
-      const response = await apiClient.get(`/agames?page=${pageNumber}&onPage=4&orderDirection=asc
+      const response = await apiClient.get(`/games?page=${pageNumber}&onPage=4&orderDirection=asc
       &orderBy=round`)
         return response.data
     } catch (error) {
